ffaf77291a501c8741f115fedaa4057a254b6bb1
Ashish Nautiyal <ashish.nautiyal102@gmail.com>
2026-08-30 20:08:21 +0530

    feat: added prodRelease-badging.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,8 @@ android {
             // To publish on the Play store a private signing key is required, but to allow anyone
             // who clones the code to sign and run the release variant, use the debug signing key.
             // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
-            signingConfig = signingConfigs.named("debug").get()
-            //signingConfig = signingConfigs.getByName("myKey")
+          //  signingConfig = signingConfigs.named("debug").get()
+            signingConfig = signingConfigs.getByName("myKey")
             // Ensure Baseline Profile is fresh for release builds.
             baselineProfile.automaticGenerationDuringBuild = true
         }
@@ -93,6 +93,7 @@ dependencies {
     implementation(projects.core.common)
     implementation(projects.core.data)
     implementation(projects.sync.work)
+    implementation(projects.core.data)
 
 
 
@@ -124,6 +125,7 @@ dependencies {
     implementation(libs.androidx.compose.material3.adaptive.navigation)
     implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.compose.runtime.tracing)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtimeCompose)
     implementation(libs.androidx.lifecycle.viewModel.navigation3)
     implementation(libs.androidx.profileinstaller)
@@ -177,7 +179,7 @@ baselineProfile {
     automaticGenerationDuringBuild = false
 
     // Make use of Dex Layout Optimizations via Startup Profiles
-    dexLayoutOptimization = false
+    dexLayoutOptimization = true
 }
 
 dependencyGuard {
